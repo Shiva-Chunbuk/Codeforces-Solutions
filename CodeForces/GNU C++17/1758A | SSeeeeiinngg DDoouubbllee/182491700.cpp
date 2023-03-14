2d684a6e51e6b4f188cb4fdb1c0bb850b30308a1
Shiva Chunbuk <127691335+shiva017@users.noreply.github.com>
2023-03-14 23:33:54 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1758A | SSeeeeiinngg DDoouubbllee/182491700.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        string miller;
+        cin>>miller;
+        cout<<miller;
+        reverse(miller.begin(),miller.end());
+        cout<<miller<<endl;
+    }
+            }
